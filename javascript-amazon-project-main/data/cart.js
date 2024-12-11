8099b2850c9240914d07f91c17b2cd1543cc249a
003falcon <333mohammedmohtasim@gmail.com>
2024-12-11 23:24:09 +0530

    Dynamic handling of cart using local Storage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,7 +1,12 @@
-export let cart=[
+export let cart=JSON.parse(localStorage.getItem('cart'))|| [
   {productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",quantity:3},
   {productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",quantity:1}]
 //export keyword is written in front of those variables to be used by other files
+
+function saveToLocalStorage()
+{
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function addProductToCart(productId,qtySelected)
 {
   let isInCart=false;
@@ -21,6 +26,7 @@ export function addProductToCart(productId,qtySelected)
           quantity:qtySelected
         });
       }
+      saveToLocalStorage();
 }
 
 export function removeFromCart(productId)
@@ -28,5 +34,6 @@ export function removeFromCart(productId)
   cart=cart.filter((item)=>{
     return item.productId!==productId;
   })
+  saveToLocalStorage();
 
 }
