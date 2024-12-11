7f75db1dbe3535512b09e5032e73531d09dc1c0c
003falcon <333mohammedmohtasim@gmail.com>
2024-12-11 14:40:58 +0530

    used products.js file

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,31 +1,45 @@
 // console.log('bismillah')
-const products=[{
-  imageLink:'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating:{
-    stars:4.5,
-    numUsers:87
-  },
-  priceCents:1090
-},
-{
-  imageLink:'images/products/intermediate-composite-basketball.jpg',
-  name:'Intermediate Size Basketball',
-  rating:{
-    stars:4,
-    numUsers:127
-  },
-  priceCents:2095
-},
-{
-  imageLink:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name:'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating:{
-    stars:4.5,
-    numUsers:56
-  },
-  priceCents:799
-}]
+
+//example of how to save data to generate html
+// const products=[
+//     {
+//     imageLink:'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//     rating:{
+//       stars:4.5,
+//       numUsers:87
+//     },
+//     priceCents:1090
+//   },
+//   {
+//     imageLink:'images/products/intermediate-composite-basketball.jpg',
+//     name:'Intermediate Size Basketball',
+//     rating:{
+//       stars:4,
+//       numUsers:127
+//     },
+//     priceCents:2095
+//   },
+//   {
+//     imageLink:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name:'Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating:{
+//       stars:4.5,
+//       numUsers:56
+//     },
+//     priceCents:799
+//   },
+//   {
+//     imageLink:'images/products/black-2-slot-toaster.jpg',
+//     name:'2 Slot Toaster - Black',
+//     rating:{
+//       stars:5,
+//       numUsers:2197
+//     },
+//     priceCents:1899
+//   }
+
+// ]
 
 let productsHTML='';
 products.forEach((product)=>{
